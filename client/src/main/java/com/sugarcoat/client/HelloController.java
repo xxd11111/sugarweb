7ee8cc2f9e50686846ccb7d7fa0893ac3bf84d63
xxd <865209186@qq.com>
2023-06-18 23:19:43 +0800

    1

---
+++ client/src/main/java/com/sugarcoat/client/HelloController.java
@@ -0,0 +1,14 @@
+package com.sugarcoat.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("哈哈，你是大傻!");
+    }
+}
---
+++ client/src/main/java/com/sugarcoat/client/SugarcoatClientApplication.java
@@ -0,0 +1,10 @@
+package com.sugarcoat.client;
+
+/**
+ * 客户端启动
+ *
+ * @author xxd
+ * @date 2023/6/18 19:42
+ */
+public class SugarcoatClientApplication {
+}

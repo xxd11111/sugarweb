ff42698c9928c73ec670569509b805b122eb1d41
xxd <865209186@qq.com>
2024-11-03 23:29:05 +0800

    提交测试

--- service-chatAssistant/src/main/java/com/sugarweb/chatAssistant/ChatAssistantApplication.java
+++ service-chatAssistant/src/main/java/com/sugarweb/chatAssistant/ChatAssistantApplication.java
@@ -2,7 +2,6 @@ package com.sugarweb.chatAssistant;
 
 import com.sugarweb.chatAssistant.agent.AgentManager;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

d32515ad116b3b677dac4032ef68c181e953f804
xxd <865209186@qq.com>
2025-08-05 17:11:46 +0800

    1

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts">
-import PcLayout from './components/PcLayout.vue'
+<template>
+  <n-config-provider :theme="theme">
+    <n-layout>
+      <n-layout-header>
+        <HeaderBar/>
+      </n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+        <RouterView />
+      </n-layout-content>
+    </n-layout>
+  </n-config-provider>
+</template>
+
+
+<script lang="ts">
+import type { GlobalTheme } from 'naive-ui'
+import { darkTheme } from 'naive-ui'
+import { defineComponent, ref } from 'vue'
+import HeaderBar from '@/components/HeaderBar.vue'
+
+export default defineComponent({
+  components: { HeaderBar: HeaderBar },
+  setup() {
+    return {
+      darkTheme,
+      theme: ref<GlobalTheme | null>(null)
+    }
+  }
+})
 </script>
 
-<template>
-  <PcLayout />
-</template>
+<style scoped>
+
+</style>
+
